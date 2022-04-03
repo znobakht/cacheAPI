17765d0f7511e0c6d738ee8b04a3ab0ae139fe3f
zakie nobakht <zakie.nobakht@gmail.com>
2022-04-03 14:53:38 +0430

    expire update in each read based on TTLAmount

--- routes/cache.router.ts
+++ routes/cache.router.ts
@@ -1,31 +1,33 @@
 import * as express from "express";
+import { appConf } from "../config/app";
 const router = express.Router();
 
 import cacheModel from "../models/cache.model";
 router.get("/:id", async (req, res) => {
-    try {
-    let cacheObject = await cacheModel.findById(req.params.id);
-      if (cacheObject) {
-        return res.json(cacheObject);
-      }
-
-      const text = (Math.random() + 1).toString(36).substring(7);
-      cacheObject = await cacheModel.create({ text });
-      return res.json(cacheObject);
-    } catch (err) {
-      console.error(err);
-      return res.status(500).send(err.message);
-    }
-});
-router.post("/create", async (req, res) => {
   try {
-      if(!req?.body?.text){
-          return res.status(400).json({message: "text is required"});
-      }
-      const text = req.body.text;
-      const cacheObject = await cacheModel.create({text});
+    let cacheObject = await cacheModel.findById(req.params.id);
+    if (cacheObject) {
+      cacheObject.expire = Date.now() + appConf.TTLAmount;
+      await cacheObject.save();
       return res.json(cacheObject);
+    }
 
+    const text = (Math.random() + 1).toString(36).substring(7);
+    cacheObject = await cacheModel.create({ _id: req.params.id ,text });
+    return res.json(cacheObject);
+  } catch (err) {
+    console.error(err);
+    return res.status(500).send(err.message);
+  }
+});
+router.post("/", async (req, res) => {
+  try {
+    if (!req?.body?.text) {
+      return res.status(400).json({ message: "text is required" });
+    }
+    const text = req.body.text;
+    const cacheObject = await cacheModel.create({ text });
+    return res.json(cacheObject);
   } catch (err) {
     console.error(err);
     return res.status(500).send(err.message);
