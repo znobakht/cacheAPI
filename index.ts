cf53171694436ec3b5c3218d788b798622296724
zakie nobakht <zakie.nobakht@gmail.com>
2022-04-03 09:35:50 +0430

    some app configuration. connecting to database and running server.

---
+++ index.ts
@@ -0,0 +1,23 @@
+import { connect } from "mongoose";
+import { mongoConf } from "./config/mongo";
+import { appConf } from "./config/app";
+import cacheRouter from "./routes/cache.router";
+import * as express from "express";
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+app.use("/cache", cacheRouter);
+connect(mongoConf.dbAddress)
+  .then(() => {
+    console.log(`connected to mongo at ${mongoConf.dbAddress} address`);
+  })
+  .catch((err) => {
+    console.error(err);
+  });
+
+app.listen(appConf.port, () => {
+  console.log(`server is running on port ${appConf.port}`);
+});
